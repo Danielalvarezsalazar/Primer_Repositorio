37817bf744a57b97a077579f7c91cbc326b7de55
Danielalvarezsalazar <angel.alvarez.salazar@cbtis89.edu.mx>
2024-10-21 09:28:50 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,12 @@
+<?php
+/*CBTIS 89
+P00Presentacion
+Programa que contiene mis datos personales
+Angel Daniel Alvarez Salazar
+3°A Programacion Matutino*/
+
+echo "Mi nombre es Angel Daniel Alvarez Salazar";
+echo "<br>"," Estudio en el CBTIS 89";
+echo "<br>","En 3er semestre de la carrera de Programacion A Matutino";
+echo "<br>","Vivo en la ciudad de Durango, DGO.";
+?>
